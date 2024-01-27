ceeaeff62fe80393c8335f0c57cecfea65089c83
Matheus Silva <19240911+c4lib4n@users.noreply.github.com>
2024-01-27 15:25:47 +0000

    Update App.java

--- Bitcoin_Project/Bitcoin/src/App.java
+++ Bitcoin_Project/Bitcoin/src/App.java
@@ -1,7 +1,26 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class App {
-    public static void main(String[] args) {
 
-        }
-    }
+	public static void main(String[] args) {
+		
+		BlockChain blockChain = new BlockChain();
+		Miner miner = new Miner();
+		
+		// we can create the blocks one by one
+		Block block0 = new Block(0, "", Constants.GENESIS_PREV_HASH);
+		miner.mine(block0, blockChain);
+		
+		Block block1 = new Block(1, "transaction1", blockChain.getBlockChain().
+				get(blockChain.getSize()-1).getHash());
+		miner.mine(block1, blockChain);
+		
+		Block block2 = new Block(2, "transaction2", blockChain.getBlockChain().
+				get(blockChain.getSize()-1).getHash());
+		miner.mine(block2, blockChain);
+		
+		System.out.println("\n" + "BLOCKCHAIN:\n"+blockChain);
+		System.out.println("Miner's reward: " + miner.getReward());
+		
+		// a single block is mined after every 10 mins
+	}
+
+}

25fc268c6f279847cd9f48b7c0b0af2bc98ef621
Matheus Silva <19240911+c4lib4n@users.noreply.github.com>
2024-01-27 15:27:59 +0000

    Update SHA256Helper.java

--- Bitcoin_Project/Bitcoin/src/SHA256Helper.java
+++ Bitcoin_Project/Bitcoin/src/SHA256Helper.java
@@ -1,25 +1,28 @@
+
 import java.security.MessageDigest;
 
 public class SHA256Helper {
 
-    public static String generateHash(String data){
-        try{
+	public static String generateHash(String data) {
+		try {		
+			MessageDigest digest = MessageDigest.getInstance("SHA-256");
+			byte[] hash = digest.digest(data.getBytes("UTF-8"));
 
-            MessageDigest digest = MessageDigest.getInstance("SHA-256");
-            byte[] hash = digest.digest(data.getBytes("UTF-8"));
-
-            // we  want to end up with hexadecimal values not bytes
-            StringBuffer hexadecimalString = new StringBuffer();
-
-            for (int i = 0; i < hash.length; i++){
-                String hexadecimal = Integer.toHexString(0xff & hash[i]);
-                hexadecimalString.append(hexadecimal);
-            }
-
-            return hexadecimalString.toString();
-        } catch (Exception e){
-            throw new RuntimeException(e);
-        }
-    }
-
+			//we want to end up with hexadecimal values not bytes
+			StringBuilder hexadecimalString = new StringBuilder();
+		
+			for (int i = 0; i < hash.length; i++) {
+				String hexadecimal = Integer.toHexString(0xff & hash[i]);
+				// padding !!!
+				if(hexadecimal.length() == 1) hexadecimalString.append('0');
+				hexadecimalString.append(hexadecimal);
+			}
+			
+			return hexadecimalString.toString();
+			
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
 }
+

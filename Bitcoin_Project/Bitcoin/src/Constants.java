a2124ef6ddc12b2264a0a84b478f441b235b706b
Matheus Silva <19240911+c4lib4n@users.noreply.github.com>
2024-01-27 15:27:22 +0000

    Update Constants.java

--- Bitcoin_Project/Bitcoin/src/Constants.java
+++ Bitcoin_Project/Bitcoin/src/Constants.java
@@ -6,7 +6,7 @@ public class Constants {
     public static final int DIFFICULTY =1;
     // this is the reward in the underlying cryptocurrency
     public static final int REWARD = 10;
-
+    // first block (genesis)
     public static final String GENESIS_PREV_HASH = "0000000000000000000000000000000000000000000000000000000000000000";
 
 }

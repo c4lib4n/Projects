069d9a8e5e51f0db62188ec506644a21c17ec030
chagasda <matheussilva_51@hotmail.com>
2024-07-31 13:05:31 +0100

    First exercise in RUST.

---
+++ Rust/ExerciseA/target/debug/ExerciseA.d
@@ -0,0 +1,1 @@
+C:\Users\chagasda\Documents\Study\Projects\Rust\ExerciseA\target\debug\ExerciseA.exe: C:\Users\chagasda\Documents\Study\Projects\Rust\ExerciseA\src\main.rs
---
+++ Rust/ExerciseA/target/debug/deps/ExerciseA.d
@@ -0,0 +1,5 @@
+C:\Users\chagasda\Documents\Study\Projects\Rust\ExerciseA\target\debug\deps\ExerciseA.exe: src/main.rs
+
+C:\Users\chagasda\Documents\Study\Projects\Rust\ExerciseA\target\debug\deps\ExerciseA.d: src/main.rs
+
+src/main.rs:

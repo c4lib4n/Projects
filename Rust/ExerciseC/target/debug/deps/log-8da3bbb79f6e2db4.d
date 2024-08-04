be0eb901e0ca1c5ce8a699c68f54df7dfcd6f565
chagasda <matheussilva_51@hotmail.com>
2024-08-04 07:32:53 +0100

    Exercise C

---
+++ Rust/ExerciseC/target/debug/deps/log-8da3bbb79f6e2db4.d
@@ -0,0 +1,10 @@
+C:\Users\chagasda\Documents\Study\Projects\Rust\ExerciseC\target\debug\deps\liblog-8da3bbb79f6e2db4.rmeta: C:\Users\chagasda\.cargo\registry\src\index.crates.io-6f17d22bba15001f\log-0.4.22\src\lib.rs C:\Users\chagasda\.cargo\registry\src\index.crates.io-6f17d22bba15001f\log-0.4.22\src\macros.rs C:\Users\chagasda\.cargo\registry\src\index.crates.io-6f17d22bba15001f\log-0.4.22\src\serde.rs C:\Users\chagasda\.cargo\registry\src\index.crates.io-6f17d22bba15001f\log-0.4.22\src\__private_api.rs
+
+C:\Users\chagasda\Documents\Study\Projects\Rust\ExerciseC\target\debug\deps\liblog-8da3bbb79f6e2db4.rlib: C:\Users\chagasda\.cargo\registry\src\index.crates.io-6f17d22bba15001f\log-0.4.22\src\lib.rs C:\Users\chagasda\.cargo\registry\src\index.crates.io-6f17d22bba15001f\log-0.4.22\src\macros.rs C:\Users\chagasda\.cargo\registry\src\index.crates.io-6f17d22bba15001f\log-0.4.22\src\serde.rs C:\Users\chagasda\.cargo\registry\src\index.crates.io-6f17d22bba15001f\log-0.4.22\src\__private_api.rs
+
+C:\Users\chagasda\Documents\Study\Projects\Rust\ExerciseC\target\debug\deps\log-8da3bbb79f6e2db4.d: C:\Users\chagasda\.cargo\registry\src\index.crates.io-6f17d22bba15001f\log-0.4.22\src\lib.rs C:\Users\chagasda\.cargo\registry\src\index.crates.io-6f17d22bba15001f\log-0.4.22\src\macros.rs C:\Users\chagasda\.cargo\registry\src\index.crates.io-6f17d22bba15001f\log-0.4.22\src\serde.rs C:\Users\chagasda\.cargo\registry\src\index.crates.io-6f17d22bba15001f\log-0.4.22\src\__private_api.rs
+
+C:\Users\chagasda\.cargo\registry\src\index.crates.io-6f17d22bba15001f\log-0.4.22\src\lib.rs:
+C:\Users\chagasda\.cargo\registry\src\index.crates.io-6f17d22bba15001f\log-0.4.22\src\macros.rs:
+C:\Users\chagasda\.cargo\registry\src\index.crates.io-6f17d22bba15001f\log-0.4.22\src\serde.rs:
+C:\Users\chagasda\.cargo\registry\src\index.crates.io-6f17d22bba15001f\log-0.4.22\src\__private_api.rs:

1dd45fea4372ba9130f5d0bf7b43befd7df3f6fe
Matheus SIlva <matheussilva_51@hotmail.com>
2025-03-30 08:59:48 +0100

    Add barcode generator project to print ASIN label on Zebra ZD621 printers.

---
+++ Java/Barcode/Barcode/src/main/java/org/example/BarcodeGenerator.java
@@ -0,0 +1,13 @@
+import javax.swing.SwingUtilities;
+
+public class BarcodeGenerator {
+
+    public static void main(String[] args) {
+
+        SwingUtilities.invokeLater(() -> {
+            BarcodeGeneratorGUI gui = new BarcodeGeneratorGUI();
+            gui.setVisible(true);
+        });
+        //just a random comment
+    }
+}
